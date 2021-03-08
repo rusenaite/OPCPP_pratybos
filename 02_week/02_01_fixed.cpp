2d6d036ae0522c6771e0db9f59a876159ef0356f
Austėja Rušėnaitė <79420654+rusenaite@users.noreply.github.com>
2021-03-08 02:41:28 +0200

    Update 02_01_fixed.cpp

--- 02_week/02_01_fixed.cpp
+++ 02_week/02_01_fixed.cpp
@@ -1,11 +1,3 @@
-/*
-* Usage of iterators:
-* An iterator refers to the first element in the multiset container.
-* Bidirectional iterators are iterators that can be used to access
-* the sequence of elements in a range in both directions
-* (towards the end and towards the beginning).
-*/
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -33,7 +25,7 @@ void multiset_add(multiset &Set){
         cin >> input;
         if(Set.elements.size() == 0){
             Set.elements.push_back(input);
-            Set.counts.push_back(1); //adds 1 to the new element
+            Set.counts.push_back(1);
         } else{
             bool isDublicate = false;
             for(int j = 0; j < Set.elements.size(); ++j){
@@ -45,29 +37,12 @@ void multiset_add(multiset &Set){
             }
             if(isDublicate == false){
                 Set.elements.push_back(input);
-                Set.counts.push_back(1); //adds more to that 1 because of dublicates
+                Set.counts.push_back(1);
             }
         }
     }
 }
 
-/*
-  copy(strt_iter1, end_iter1, strt_iter2) :
-* Defined in header <algorithm>.
-* The generic copy function used to copy a range of elements from one
-  container to another.
-* It takes 3 arguments:
-
-strt_iter1 : The pointer to the beginning of the source container,
-            from where elements have to be started copying.
-end_iter1 : The pointer to the end of source container, till where
-            elements have to be copied.
-strt_iter2 : The pointer to the beginning of destination container,
-            to where elements have to be started copying.
-*/
-
-
-//to convert elements and count into a string
 void multiset_toString(multiset &Set){
     ostringstream elements;
     ostringstream counts;
@@ -82,30 +57,9 @@ void multiset_toString(multiset &Set){
     cout << "\nCounters: " << counts.str() << endl;
 }
 
-/*
-
-The stringstream, ostringstream, and istringstream objects are used for
-input and output to a string. They behave in a manner similar to fstream,
-ofstream and ifstream objects.
-
-
-An ostringstream object can be used to write to a string.
-This is similar to the C sprintf() function.
-
-An istringstream object can be used to read from a string.
-This is similar to the C sscanf() function.
-
-A stringstream object can be used for both input and output to a string.
-This is similar to the C fstream object.
-
-<< operator inserts data INTO the stream.
->> operator extracts data OUT OF a string stream.
-
-*/
-
 int main(){
-    multiset Set; //defining name of struct
-    multiset_add(Set); //passing struct as an parameter
+    multiset Set;
+    multiset_add(Set);
     multiset_toString(Set);
 
     return 0;
